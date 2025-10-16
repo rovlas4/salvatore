508fb8031854e46ecc79c61ed3ef0c084b7f9597
rovlas4 <44chava44@gmail.com>
2025-10-16 16:21:23 -0600

    fix router in home page

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
   templateUrl: './home.page.html',
-  styleUrls: ['./home.page.css']
+  styleUrls: ['./home.page.css'],
+  imports: [RouterLink]
 })
 export class HomePage {}
