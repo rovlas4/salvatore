5e6d4d0975b51e5e125cbb89655274b9de5b9362
rovlas4 <44chava44@gmail.com>
2025-10-16 16:25:21 -0600

    fix routing

--- src/app/pages/auth/login/login.page.ts
+++ src/app/pages/auth/login/login.page.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
   templateUrl: './login.page.html',
-  styleUrls: ['./login.page.css']
+  styleUrls: ['./login.page.css'],
+  imports: [RouterLink]
 })
 export class LoginPage {}
--- src/app/pages/auth/register/register.page.ts
+++ src/app/pages/auth/register/register.page.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
   templateUrl: './register.page.html',
-  styleUrls: ['./register.page.css']
+  styleUrls: ['./register.page.css'],
+  imports: [RouterLink]
 })
 export class RegisterPage {}
